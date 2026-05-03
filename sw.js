70a8bd50db08d09816b945672b476c86b02885c8
Thomas <miuuxx@gmail.com>
2026-05-03 04:23:42 +0200

    feat: restauration cloud (Sheet -> PWA) + colonne speed
    - Apps Script : route action="backup" qui renvoie sessions + weights
    - Helpers _toDate/_isoDate/_frDate defensifs (instanceof Date foire entre realms en V8)
    - Colonne speed ajoutee en col 9 (avec auto-fix du header pour les Sheets existants)
    - PWA : bouton "Restaurer depuis le cloud" dans DataModal (visible si BACKEND_URL configure)
    - SW cache bumpe muscu-v5 -> muscu-v6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "muscu-v5";
+const CACHE = "muscu-v6";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.svg", "./icon-512.svg"];
 
 self.addEventListener("install", e => {
